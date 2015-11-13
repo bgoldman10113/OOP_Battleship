a7486bcd0d55869a425a7284ba77f5726cd77aae
Brandon Goldman <brandon.m.goldman@gmail.com>
2015-11-12 22:16:54 -0500

    Create Ships.h

---
+++ include-string/Ships.h
@@ -0,0 +1,11 @@
+
+#include <string>
+
+//A struct that contains all of the information we would need about a ship when it is called in gameplay
+const int shipTypes = 5;
+
+struct shipStruct {
+    string name;
+    int length;
+    int shipValue;
+};
